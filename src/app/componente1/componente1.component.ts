08fbbd8433701ca89d8afbf1e1c4ca906a4887bf
ngzdllo <ngzdllo@gmail.com>
2022-10-28 08:52:11 -0500

    primer trabajo

---
+++ src/app/componente1/componente1.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-componente1',
+  templateUrl: './componente1.component.html',
+  styleUrls: ['./componente1.component.css']
+})
+export class Componente1Component {
+
+  constructor() { }
+
+
+}
